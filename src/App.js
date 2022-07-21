e2bb5c94bf6c7031c62c9e7c8ae3f2bf2c48ed6a
teacherakhror <akhrorteacher@gmail.com>
2022-07-21 18:37:08 +0500

    create components and add react-icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+// adding components
+import Player from './components/Player'
+import Song from './components/Song'
+
 function App() {
   return (
     <div className="App">
-      <h1>React Music App</h1>
+      <Song />
+      <Player />
     </div>
   )
 }
---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+export const Song = () => {
+  return (
+    <div>
+      <div className="song-container">
+        <h1>Pictrue</h1>
+        <h1>Song Name</h1>
+        <h1>Artist</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Song
